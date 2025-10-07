e7bb0eb5e9a95ffd76d3441d5f2f605e659bc9b5
Shaaf <mdshafeeq1997@gmail.com>
2025-10-07 17:50:01 +0530

    Data Preprocssing added

---
+++ src/Data_Ingestion(Web_Scrapping).py
@@ -0,0 +1,129 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select, WebDriverWait
+import time
+from selenium.webdriver.common.by import By
+import pandas as pd
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import os
+import logging
+
+# Setting up logging
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+
+# logging configuration
+logger = logging.getLogger('DataIngestionLogger')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'DataIngestionLogger.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+#checking if the data file already exists
+file_name = r"C:\Users\Shaaf\Desktop\Data Science\Practice Projects\Agriculture Price Prediction\Data\agmarknet_data new.xlsx"
+if os.path.exists(file_name):
+    print(f"Data File  already exists. Exiting to avoid overwriting.")
+    # exit()
+else:
+    print(f"File '{file_name}' does not exist. Proceeding with web scraping.")   
+    #IF NOT, continue with the web scraping process
+    # Create a fresh Chrome session
+    # Configure Chrome for headless mode
+    logger.info("Configuring Chrome in headless mode.")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # or "--headless=new" if it works in your setup
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    # Start Chrome session
+    logger.info("Starting a new headless Chrome session.")
+    service = Service()  # specify chromedriver path if needed
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    logger.info("Chrome session started successfully.")
+
+    # Open website
+    logger.info("Navigating to agmarknet.gov.in")
+    driver.get("https://agmarknet.gov.in/")
+    logger.info("Navigation successful.")
+
+    logger.info("Navigated to agmarknet.gov.in")
+    # Wait for page load
+    time.sleep(3)
+    # Interacting with the dropdown buttons
+    logger.info("Interacting with dropdown menus, Selecting Commodity(Ginger)")
+    select_element = driver.find_element(By.ID, "ddlCommodity") #Commodity section
+    select = Select(select_element)
+    select.select_by_value("103") #selecting the commodity(Ginger)
+    time.sleep(2)
+
+    logger.info("Selecting States")
+    # Interact with another dropdown,slecting state, district and market
+    select_element = driver.find_element(By.ID, "ddlState")
+    select = Select(select_element)
+    select.select_by_value("KK") #selecting the state(Karnataka)
+    time.sleep(3)
+
+    logger.info("Selecting District(Bangalore) and Market(Bangalore)")
+    select_element = driver.find_element(By.ID, "ddlDistrict") #District section
+    select = Select(select_element)
+    select.select_by_value("1") #selecting the district(Bagalore)
+    time.sleep(3)
+
+    select_element = driver.find_element(By.ID, "ddlMarket") #Market section
+    select = Select(select_element)
+    select.select_by_value("107") #selecting the market(Bangalore)
+    time.sleep(3)
+
+    #enter date in DD-MM-YYYY format
+    logger.info("Entering Date(01-01-2010)")
+    date_input = driver.find_element(By.ID, "txtDate") # Find the date input element
+    date_input.clear() # Clear any existing value
+    date_input.send_keys("01-01-2010") # Type the date (Selecting first available date)
+    date_input.send_keys(Keys.ENTER) # Hit Enter
+    time.sleep(3)
+
+    logger.info("Submitting the form")
+    # Print the current URL
+    print("Final URL:", driver.current_url) #Comment this line if you don't want to see the URL
+    # Wait for page load
+    time.sleep(3)
+    # Extract table data
+    data = []
+    table_rows = driver.find_elements(By.XPATH, '//*[@id="cphBody_GridPriceData"]/tbody/tr') # Locate table rows using XPath
+    print(f"Number of rows found: {len(table_rows)}") # Print number of rows found
+    logger.info(f"Number of rows found: {len(table_rows)}")
+    # Iterate through rows and extract cell data
+    logger.info("Extracting data from table, row by row, this may take a while...Please wait...")
+    for rw in table_rows:
+        cells = rw.find_elements(By.TAG_NAME, 'td')
+        row_data = []
+        for cell in cells:
+            text_data = cell.text.strip()
+            row_data.append(text_data)
+        data.append(row_data)
+    driver.quit() # Close the browser
+    # Convert to DataFrame
+    df = pd.DataFrame(data)
+   
+    logger.info("Data extraction complete.")
+    # Export to Excel
+    df.to_excel(r"C:\Users\Shaaf\Desktop\Data Science\Practice Projects\Agriculture Price Prediction\Data\agmarknet_data new.xlsx", index=False)
+
+    print("✅ Data exported to agmarknet_data.xlsx")
---
+++ src/Data_Preporcessing.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import logging
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+# Setting up logging
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+
+# logging configuration
+logger = logging.getLogger('Data_Preprocessing')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'Data_Preprocessing')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logger.info("Data Preprocessing module initialized.")
+
+#Reading the data
+data_file_path = r"C:\Users\Shaaf\Desktop\Data Science\Practice Projects\Agriculture Price Prediction\Data\agmarknet_data new.xlsx"
+try:
+    """Loads the data from Excel file."""
+    df = pd.read_excel(data_file_path)
+    logger.info(f"Data loaded successfully from {data_file_path}")
+except Exception as e:
+    logger.error(f"Error loading data: {e}")
+    raise
+# Display basic information about the dataset
+logger.info(f"Dataset shape: {df.shape}")
+
+#Checking for missing values
+missing_values = df.isnull().sum()
+
+# Handling missing values
+# Dropping rows with missing values for simplicity
+logger.info("Checking for missing values in each column:")
+if missing_values.any():
+    logger.info("Missing values found:Removing rows with missing values.")
+    df.dropna(inplace=True)
+    logger.info("Dropped rows with missing values.")
+    current_missing_values = df.isnull().sum()
+    logger.info(f"Current missing values after dropping: {current_missing_values}")
+else:
+    logger.info("No missing values found.")
+
+# Checking for duplicate rows
+duplicate_rows = df.duplicated().sum()
+if duplicate_rows > 0: # If there are duplicate rows
+    logger.info(f"Found {duplicate_rows} duplicate rows. Removing duplicates.")
+    df.drop_duplicates(inplace=True)
+    logger.info("Duplicate rows removed.")
+else:
+    logger.info("No duplicate rows found.")
+
+df.rename(columns={0:'Sl.No',1:'District Name',2:'Market Name',3:'Commodity',4:'Variety',5:'Grade',6:'Min Price',7:'Max Price',8:'Avg Price',9:'Date'}, inplace=True) 
+logger.info(f"Found columns : \n{df.columns}")   
+# Display basic statistics of the dataset
+# logger.info(f"Dataset statistics:\n{df.describe()}")
+print("Checking the different Grades of commodities available in the dataset")
+print(df['Grade'].value_counts())
+print("Selecting only 'FAQ' variety for the analysis")
+df = df[df['Grade'] == 'FAQ']
+
+print("As we have 3 different prices,let's plot and see the differences.")
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+min_resampled=df['Min Price'].resample('ME').mean() # Minimum price Monthly resampling
+max_resampled=df['Max Price'].resample('ME').mean() # Maximum price Monthly resampling
+avg_resampled=df['Avg Price'].resample('ME').mean() # Average price Monthly resampling
+
+#let's set up plot directory
+plot_dir = 'plots'
+os.makedirs(plot_dir, exist_ok=True)
+
+plt.figure(figsize=(15,6))
+plt.plot(min_resampled, label='Min Price', color='blue')
+plt.plot(max_resampled, label='Max Price', color='red')
+plt.plot(avg_resampled, label='Avg Price', color='green')
+plt.title('Monthly Resampled Prices')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.savefig(os.path.join(plot_dir, 'weekly_prices.png'))
+plt.show()
+plt.close()
+
+print("Selecting Average Price for the analysis")
+df = df[['Avg Price']]
+df.rename(columns={'Avg Price':'Price'}, inplace=True)
+logger.info(f"Data after selecting 'FAQ' grade and 'Avg Price':\n{df.head()}")
+df.sort_index(inplace=True)
+# Checking for outliers using boxplot
+df=df.resample('W').mean() # Weekly resampling
+plt.figure(figsize=(8,5))
+sns.boxplot(x=df['Price'])
+plt.title('Boxplot of Prices')
+plt.show()
+plt.figure(figsize=(15,6))
+plt.plot(df['Price'], label='Price', color='blue')
+plt.title('Weekly Resampled Average Prices')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.savefig(os.path.join(plot_dir, 'Weekly Resampled Average Prices.png'))
+plt.show()
+
+#There are not entries between 2012 to 2015
+#So we will consider data from 2016 to till date
+df=df[df.index >= '2016-01-01']
+# df=df.to_frame()
+#for random missing values we can use interpolation
+df['Price'].interpolate(method='linear', inplace=True)
+plt.plot(figsize=(15,6))
+plt.plot(df['Price'], label='Price', color='blue')
+plt.title('Weekly Resampled Average Prices from 2016 onwards')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.legend()
+plt.savefig(os.path.join(plot_dir, 'Weekly Resampled Average Prices from 2016 onwards.png'))
+plt.show()
+logger.info(f"Data after filtering from 2016 onwards:\n{df.head()}")
+# Checking for stationarity using ADF test
+
+result = adfuller(df['Price'])
+logger.info('ADF Statistic: %f' % result[0])
+logger.info('p-value: %f' % result[1])
+pvalue=result[1]
+if pvalue <= 0.05:
+    logger.info("The time series is stationary.")
+else:
+    logger.info("The time series is non-stationary, applying differencing.")
+
+#checking for seasonality and trend using seasonal decomposition
+decomposition = seasonal_decompose(df['Price'], model='additive', period=52)
+decomposition.plot()
+plt.show()
+
+#applying log transformation
+df['Price'] = np.log(df['Price'])
+plt.figure(figsize=(15,6))
+plt.plot(df)
+plt.title('Log Transformed Weekly Resampled Average Prices from 2016 onwards')
+plt.xlabel('Date')
+plt.ylabel('Log Price')
+plt.legend()    
+plt.savefig(os.path.join(plot_dir, 'Log Transformed Weekly Resampled Average Prices from 2016 onwards.png'))
+plt.show()
+df.to_csv(r"C:\Users\Shaaf\Desktop\Data Science\Practice Projects\Agriculture Price Prediction\Data\preprocessed_data.csv")
+logger.info("Preprocessed data saved to 'preprocessed_data.csv'.")
